a566d2f16e555c9db62b6d63b4d0cd2f710ad3ba
matt190589 <matthew.doherty97@gmail.com>
2024-07-23 22:30:33 +0100

    icons added and mobile design changes added

--- src/app/components/NavbarBottom.tsx
+++ src/app/components/NavbarBottom.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
+import Image from 'next/image';
 
 const NavBarBottom: React.FC = () => {
     return (
         <footer className="fixed inset-x-0 bottom-0 bg-bf-navy shadow-lg p-4 border-t border-bf-pink">
             <div className="flex justify-around items-center">
-                <a href="#" className="text-bf-off-white hover:text-bf-pink">
-                    Home
+                <a href="#" className="flex text-bf-off-white hover:text-bf-pink">
+                    <div>
+                        <Image
+                            src="/Images/Icons/home-original.svg"
+                            alt="home icon"
+                            width={50}
+                            height={50}
+                        />
+                    </div>
                 </a>
-                <a href="#" className="text-bf-off-white hover:text-bf-pink">
-                    Stats
+                <a href="#" className="flex text-bf-off-white hover:text-bf-pink">
+                    <div>
+                        <Image
+                            src="/Images/Icons/record-original.svg"
+                            alt="record icon"
+                            width={50}
+                            height={50}
+                        />
+                    </div>
                 </a>
-                <a href="#" className="text-bf-off-white hover:text-bf-pink">
-                    Record
+                <a href="#" className="flex text-bf-off-white hover:text-bf-pink">
+                    <div>
+                        <Image
+                            src="/Images/Icons/stats-original.svg"
+                            alt="stats icon"
+                            width={50}
+                            height={50}
+                        />
+                    </div>
                 </a>
             </div>
         </footer>
--- src/app/components/NavbarTop.tsx
+++ src/app/components/NavbarTop.tsx
@@ -9,7 +9,15 @@ interface NavbarTopProps {
 const NavbarTop: React.FC<NavbarTopProps> = ({ title = "BabyFirsts" }) => {
     return (
         <nav className="bg-bf-navy p-4 shadow-md border-b-2 border-bf-pink">
-            <div className="container mx-auto flex justify-center md:justify-between items-center">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="flex items-start text-bf-off-white md:hidden">
+                    <Image
+                        src="/Images/Icons/menu.svg"
+                        alt="home icon"
+                        width={50}
+                        height={50}
+                    />
+                </div>
                 <div className="flex items-center justify-center">
                     <Image
                         src="/Images/baby-first-full-logo.webp"
